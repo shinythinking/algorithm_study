91a53128dadb2e8785cb77ec116140483886093c
Sa-In <162949791+shinythinking@users.noreply.github.com>
2024-04-22 11:05:13 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 13.00 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(t: String, p: String): Int {
+        var answer: Int = 0
+        for(i in 0..(t.length - p.length)){
+            if(t.substring(i until i + p.length) <= p)
+                answer++
+        }
+        return answer
+    }
+}
