ce8dcd123d12c54a567780f03c16c7bc5a5904a1
Sa-In <162949791+shinythinking@users.noreply.github.com>
2024-05-07 00:37:00 +0900

    [Silver III] Title: 햄버거 분배, Time: 168 ms, Memory: 23840 KB -BaekjoonHub

---
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.kt
@@ -0,0 +1,30 @@
+import java.util.*
+import java.io.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val(sequence, reach) = readLine()!!.split(' ').map { it.toInt() }
+    val burgerAndPerson = readLine().toCharArray()
+
+    var lastEaten = -1
+    var answer = 0
+    for((i, ch) in burgerAndPerson.withIndex()) {
+        if(ch == 'P'){
+            var from = i -reach
+            var to = i + reach
+            if(i - reach <= lastEaten)
+                from = lastEaten + 1
+
+            if(to >= burgerAndPerson.size)
+                to = burgerAndPerson.lastIndex
+
+            for(j in from..to){
+                if(burgerAndPerson[j] == 'H') {
+                    burgerAndPerson[j] = 'E'
+                    answer++
+                    break
+                }
+            }
+        }
+    }
+    println(answer)
+}
