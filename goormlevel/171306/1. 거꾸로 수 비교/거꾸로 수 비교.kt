953fc3ed5c96a8d3649203c542753612660d40b4
Sa-In <162949791+shinythinking@users.noreply.github.com>
2024-06-14 14:02:43 +0900

    [goormlevel] Title: 거꾸로 수 비교, Time: 0.15 ms, Memory: 29.33 MB, Difficulty: 1 -BaekjoonHub

--- goormlevel/171306/1. 거꾸로 수 비교/거꾸로 수 비교.kt
+++ goormlevel/171306/1. 거꾸로 수 비교/거꾸로 수 비교.kt
@@ -3,7 +3,8 @@ import java.util.*
 
 fun main(args: Array<String>) = with(BufferedReader(InputStreamReader(System.`in`))) {
 	val (left, right) = readLine().split(' ')
-	val l = left.reversed().trimStart('0').toInt()
-	val r = right.reversed().trimStart('0').toInt()
-	print(if(l <= r) right else left)
+	val leftS = left.reversed().toInt()
+	val rightS = right.reversed().toInt()
+	
+	print(if(leftS >= rightS) left else right)
 }
