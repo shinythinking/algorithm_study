18578d40bf279fbffb561c8ff26309976f0b66bc
Sa-In <162949791+shinythinking@users.noreply.github.com>
2025-01-03 21:24:25 +0900

    [Silver II] Title: Maximum Bishop, Time: 496 ms, Memory: 187048 KB -BaekjoonHub

---
+++ 백준/Silver/21869. Maximum Bishop/Maximum Bishop.kt
@@ -0,0 +1,19 @@
+package shinythinking
+
+fun main() {
+    val num = readln().toInt()
+    if(num == 1){
+        println("1\n1 1")
+    } else {
+        val answer = buildString{
+            appendLine((num - 1) * 2)
+            for (i in 2..num - 1){
+                appendLine("1 $i")
+                appendLine("$num $i")
+            }
+            appendLine("1 1")
+            appendLine("1 $num")
+        }
+        println(answer)
+    }  
+}
