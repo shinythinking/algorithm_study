f5ad413922d7775c3901ca2047eb0a988378e17b
Sa-In <162949791+shinythinking@users.noreply.github.com>
2025-02-24 01:17:17 +0900

    [Silver II] Title: 개표, Time: 912 ms, Memory: 166288 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24913. 개표/개표.kt
+++ 백준/Silver/24913. 개표/개표.kt
@@ -1,19 +1,26 @@
 fun main() {
     val reader = System.`in`.bufferedReader()
     val (numOfCandidate, num) = reader.readLine().split(" ").map { it.toInt() }
-    val votes = IntArray(numOfCandidate + 2)
+    val votes = LongArray(numOfCandidate + 2)
+    var sumOfOthers = 0L
+    var maxVote = 0L
     val answer = buildString {
         repeat(num) {
             val (opt, numOfVotes, candidate) = reader.readLine().split(" ").map { it.toInt() }
             if (opt == 1) {
-                votes[candidate] += numOfVotes
+                votes[candidate] += numOfVotes.toLong()
+                if(candidate != numOfCandidate + 1) {
+                    sumOfOthers += numOfVotes
+                    maxVote = maxOf(maxVote, votes[candidate])
+                }
             } else {
-                val jung = (votes[numOfCandidate + 1] + numOfVotes)
-                val others = Math.ceil(((votes.dropLast(1).sum() + candidate) / numOfCandidate.toDouble()))
-                if(jung > others) {
-                    appendLine("YES")
-                } else {
-                    appendLine("NO")
+                if(maxVote >= votes[numOfCandidate + 1] + numOfVotes) { appendLine("NO") }
+                else {
+                    if(sumOfOthers + candidate <= (numOfCandidate) * (votes[numOfCandidate + 1] + numOfVotes - 1)) {
+                        appendLine("YES")
+                    } else {
+                        appendLine("NO")
+                    }
                 }
             }
         }
