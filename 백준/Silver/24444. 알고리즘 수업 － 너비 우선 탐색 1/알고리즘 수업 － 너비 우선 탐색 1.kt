6bf58648f88306c9bced9c9de7b83181090fde32
Sa-In <162949791+shinythinking@users.noreply.github.com>
2025-03-24 02:12:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1148 ms, Memory: 139068 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+fun main() {
+    val reader = System.`in`.bufferedReader()
+    val (v, e, s) = reader.readLine().split(" ").map { it.toInt() }
+    val matrix = Array(v + 1) {
+        mutableListOf<Int>()
+    }
+    repeat(e) {
+        val (from, to) = reader.readLine().split(" ").map { it.toInt() }
+        matrix[from].add(to)
+        matrix[to].add(from)
+    }
+    for (i in 1..v) matrix[i].sort()
+
+    val queue = LinkedList<Int>()
+    val visited = Array(v + 1) { false }
+    queue.addLast(s)
+    visited[s] = true
+    val answer = IntArray(v + 1) { 0 }
+    var ans = 0
+    while (queue.isNotEmpty()) {
+        val polled = queue.removeFirst()
+        ans++
+        answer[polled] = ans
+        for (node in matrix[polled]) {
+            if (visited[node]) continue
+            visited[node] = true
+            queue.addLast(node)
+        }
+    }
+    println(answer.drop(1).joinToString("\n"))
+}
