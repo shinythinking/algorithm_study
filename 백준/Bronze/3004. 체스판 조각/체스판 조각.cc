6f780b09c9dae614991dad6d5a6c7a80bbb9f867
Sa-In <162949791+shinythinking@users.noreply.github.com>
2025-11-16 20:48:43 +0900

    [Bronze III] Title: 체스판 조각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3004. 체스판 조각/체스판 조각.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = 1;
+    int a = 1;
+    for(int i = 0; i < n ; i++){
+        ans += a;
+        if(i % 2 ==0){
+            a += 1;
+        }
+    }
+    cout << ans;
+}
