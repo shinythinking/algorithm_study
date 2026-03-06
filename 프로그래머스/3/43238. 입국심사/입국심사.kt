4326cc072ea114dd925bb1557e4334f86bc2bad1
Sa-In <162949791+shinythinking@users.noreply.github.com>
2026-03-06 23:01:11 +0900

    [level 3] Title: 입국심사, Time: 68.68 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.kt
+++ 프로그래머스/3/43238. 입국심사/입국심사.kt
@@ -1,28 +1,6 @@
 class Solution {
     fun solution(n: Int, times: IntArray): Long {
         var answer: Long = 0
-        fun cal(time: Long, times: IntArray): Long {
-            var ans = 0L
-            for (t in times) {
-                ans += time/t
-            }
-            return ans
-        }
-
-        fun lowerbound(target: Long, end: Long, times: IntArray): Long {
-            var left = 0L
-            var right = end + 1
-            while(left < right) {
-                val mid = left + (right - left)/2
-                if(target <= cal(mid, times)){
-                    right = mid
-                } else {
-                    left = mid + 1
-                }
-            }
-            return left
-        }
-        val maxTime = times.maxOrNull()!!.toLong() * n
-        return lowerbound(n.toLong(), maxTime, times)
+        return answer
     }
 }
